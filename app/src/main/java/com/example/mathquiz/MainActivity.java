7820d62b3b6519fc38cb6c537cc3980616ac8fcf
Yury Febenchuk <jedi.jura12@gmail.com>
2022-11-28 19:14:29 +0300

    commit 6

--- app/src/main/java/com/example/mathquiz/MainActivity.java
+++ app/src/main/java/com/example/mathquiz/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -26,10 +27,14 @@ public class MainActivity extends Activity {
     }
 
     private void setupUI() {
+        MyClicker myClicker = new MyClicker();
         problem = findViewById(R.id.problem);
         text1 = findViewById(R.id.text1);
+        text1.setOnClickListener(myClicker);
         text2 = findViewById(R.id.text2);
+        text2.setOnClickListener(myClicker);
         text3 = findViewById(R.id.text3);
+        text3.setOnClickListener(myClicker);
     }
 
     private void generateAnswers() {
@@ -38,17 +43,26 @@ public class MainActivity extends Activity {
             text1.setText(mathGenerator.getResult());
             text2.setText(mathGenerator.getNoiseResult());
             text3.setText(mathGenerator.getNoiseResult());
-        }
-        else if(position == 2) {
+        } else if (position == 2) {
             text1.setText(mathGenerator.getNoiseResult());
             text2.setText(mathGenerator.getResult());
             text3.setText(mathGenerator.getNoiseResult());
-        }
-        else if(position == 3) {
+        } else if (position == 3) {
             text1.setText(mathGenerator.getNoiseResult());
             text2.setText(mathGenerator.getNoiseResult());
             text3.setText(mathGenerator.getResult());
         }
+    }
+
+    class MyClicker implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            TextView textView = (TextView) {
+                String text = text.View.getText().toString();
+                if (text1.equals(mathGenerator.getResult(R.color.red));
+                else
+                    v.set
+
     }
 
 }
---
+++ app/src/main/java/com/example/mathquiz/MyClicker.java
@@ -0,0 +1,11 @@
+package com.example.mathquiz;
+
+import android.util.Log;
+import android.view.View;
+
+public class MyClicker implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        Log.d("RRR","RRR");
+    }
+}
